8a849b0df88ce90285156dd61e9848f720ef3c30
wuv21 <wuv21@uw.edu>
2022-03-08 22:19:01 -0500

    added workshop 07 code

---
+++ workshop-07-r-basics/introToR.R
@@ -0,0 +1,85 @@
+# Welcome to Workshop 07 - Intro to R.
+
+# Today we'll be learning the basics of R as suggested by Hadley Wickham
+# https://gist.github.com/hadley/6734639
+
+# For this lecture, we will familiarize and practice several topics:
+
+###############################################################################
+# what is R
+###############################################################################
+
+###############################################################################
+# IDE of choice and what do things do in RStudio
+###############################################################################
+
+###############################################################################
+# variable assignment 
+# and basic data types  (character, double, integer, logical)
+# and type casting
+###############################################################################
+
+
+###############################################################################
+# basic data structures (vector, matrix, list, data frame)
+###############################################################################
+
+
+###############################################################################
+# what is NULL?
+###############################################################################
+# from r documentation
+# https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/NULL
+
+# NULL represents the null object in R:
+# it is a reserved word. NULL is often returned by expressions and functions whose value is undefined.
+
+
+###############################################################################
+# what is a factor?
+###############################################################################
+# from r documentation
+# https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/factor
+
+# factor returns an object of class "factor" which has a set of integer codes the length of x
+# with a "levels" attribute of mode character and unique (!anyDuplicated(.)) entries.
+# If argument ordered is true (or ordered() is used) the result has class c("ordered", "factor").
+
+
+###############################################################################
+# comparison operators
+# >, <, ==, <=, >=, !=
+###############################################################################
+
+
+###############################################################################
+# boolean operators and the notion of short-circuiting
+# &, &&, |, || 
+###############################################################################
+
+
+###############################################################################
+# control flows: if/else statements
+###############################################################################
+
+
+###############################################################################
+# control flows: for loops
+###############################################################################
+
+
+###############################################################################
+# modularity: functions
+###############################################################################
+
+
+###############################################################################
+# control flows: apply family: sapply and lapply
+###############################################################################
+# lapply returns a list
+# sapply returns a vector
+
+
+###############################################################################
+# input/output
+###############################################################################
